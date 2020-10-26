eab3ae9497387b2438dcb66f3c778bafacecfd74
Pedro Ghiotti Martins <pedro.ghiotti.m@gmail.com>
2020-10-26 19:36:59 -0300

    Exercícios adicionados

---
+++ POO_C#/14-09-2020_Exercício_Coversor_Dólar/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercício_Static_14_09_2020
+{
+    public static class Converter
+    {
+        public static double ConvertDollarToReal(double valueInDollars, double conversionRate)
+        {
+            double valueInReais = valueInDollars * conversionRate;
+            double tax = valueInReais * 0.06;
+            return (valueInReais + tax);
+        }
+    }
+}
---
+++ POO_C#/19-10-2020_Exercício_Média_Alturas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _19_10_2020_Revisão_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; // Número de valores que o usuário deseja introduzir.
+            float[] heights; // Array que guarda os valores introduzidos.
+            float totalHeight = 0; // Guarda a soma dos valores.
+
+            Console.WriteLine("Qual o número de alturas que deseja tirar a média?");
+            n = int.Parse(Console.ReadLine());
+            heights = new float[n]; // Inicializa o array com o tamanho definido pelo usuário.
+
+            for (int i = 0; i < n; i++)  // Pede ao usuário que introduza o valor de cada um dos elementos do array definido.
+            {
+                Console.Write("Altura " + (i + 1) + ":");
+                heights[i] = float.Parse(Console.ReadLine());
+            }
+
+            foreach (var element in heights) // Soma cada valor dentro do array ao total.
+            {
+                totalHeight += element;
+            }
+
+            Console.WriteLine("\nA média das alturas dadas é " + (totalHeight / n)); // Escreve o resultado dado por dividir a soma das alturas dadas pelo seu número.
+        }
+    }
+}
