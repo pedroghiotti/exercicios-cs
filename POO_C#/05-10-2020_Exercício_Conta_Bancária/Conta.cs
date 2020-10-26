96565f97a0f173d26381cfbc246535fe3b9140f0
Pedro Ghiotti Martins <pedro.ghiotti.m@gmail.com>
2020-10-26 10:55:07 -0300

    Exercício Adicionado

---
+++ POO_C#/05-10-2020_Exercício_Conta_Bancária/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _05_10_2020_Exercício_Conta_Bancária
+{
+    class Conta
+    {
+        int accountNumber;
+        string name;
+        double balance;
+
+        public override string ToString()
+        {
+            Console.Clear();
+            return "Informações atualizadas da conta:\n\nNúmero da conta: " + accountNumber + "\nNome do titular: " + name + "\nBalanço: " + balance;
+        }
+        public void Registrar()
+        {
+            Console.WriteLine("Insira seus dados para registrar a conta.");
+            Console.Write("Nome do titular: ");
+            name = Console.ReadLine();
+            Console.Write("Número da conta: ");
+            accountNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Gostaria de fazer um depósito inicial? [ S / N ]");
+            string choice = Console.ReadLine().ToLower();
+            if (choice == "s")
+            {
+                Deposito();
+            }
+        }
+        public void AlterarNome()
+        {
+            Console.Write("Novo nome a ser usado: ");
+            name = Console.ReadLine();
+        }
+        public void Deposito()
+        {
+            Console.Write("Valor do depósito: ");
+            balance += double.Parse(Console.ReadLine());
+        }
+        public void Saque()
+        {
+            Console.Write("Valor do saque: ");
+            balance -= 5;
+            balance -= double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ POO_C#/05-10-2020_Exercício_Conta_Bancária/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _05_10_2020_Exercício_Conta_Bancária
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            conta.Registrar();
+            Console.WriteLine(conta);
+
+            int select = 0; // Variável que guarda o Input do usuário.
+            while (select != 4) // Loop que permite o usuário escolher o que deseja fazer no programa. Funciona como uma espécie de menu.
+            {
+                Console.WriteLine("\nSelecione a opção que deseja:\n\n1: Depósito.\n2: Saque.\n3: Alterar nome titular.\n4: Sair.");
+                int.TryParse(Console.ReadLine(), out int result); // Retorna 0 caso o Input não seja do tipo requisitado (int).
+                select = result;
+                switch (select) // Chama o método equivalente à opção escolhida pelo usuário.
+                {
+                    case 1:
+                        conta.Deposito();
+                        Console.WriteLine(conta);
+                    break;
+                    case 2:
+                        conta.Saque();
+                        Console.WriteLine(conta);
+                    break;
+                    case 3:
+                        conta.AlterarNome();
+                        Console.WriteLine(conta);
+                    break;
+                    default:
+                        Console.Clear();
+                    break;
+                }
+            }
+        }
+    }
+}
